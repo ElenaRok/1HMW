0b37f56388b3b54aaf6d59617af3029aef45fa7e
Elene <L.Rokolunskaya@Gmail.com>
2022-12-22 20:51:35 +0300

    h10

---
+++ h10/data.js
@@ -0,0 +1,26 @@
+      const dataJs = `[
+        {
+           "quote_id":1,
+           "quote":"I am not in danger, Skyler. I am the danger!",
+           "author":"Walter White",
+           "series":"Breaking Bad"
+        },
+        {
+           "quote_id":2,
+           "quote":"Stay out of my territory.",
+           "author":"Walter White",
+           "series":"Breaking Bad"
+        },
+        {
+           "quote_id":3,
+           "quote":"IFT",
+           "author":"Skyler White",
+           "series":"Breaking Bad"
+        }] `
+       
+             
+                         
+        
+                            
+    
+        
