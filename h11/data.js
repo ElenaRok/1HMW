cb0bbd7601d6cecb629995234d12aea25dbf7e5e
Elene <L.Rokolunskaya@Gmail.com>
2022-12-25 12:40:22 +0300

    h11

---
+++ h11/data.js
@@ -0,0 +1,21 @@
+const dataJson = [
+    {
+        "img": "img/product1.jpg",
+        "subtitle":"ellery x m'o capsule",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+        
+        },
+        {
+            "img": "img/product2.pngp",
+        "subtitle":"ellery x m'o capsule",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+            },
+            {
+                "img": "img/product3.pngp",
+        "subtitle":"ellery x m'o capsule",
+        "desc": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+        "price":"$52.00",
+                },
+]
